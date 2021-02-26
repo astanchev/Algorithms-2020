a6fcdaa206c415a26903dfca1850ddc53b94e7b9
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-02-26 14:44:05 +0200

    Add 01. Recursive Array Sum -  solution

--- 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/01_Recursive_Array_Sum/Program.cs
+++ 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/01_Recursive_Array_Sum/Program.cs
@@ -1,12 +1,28 @@
 ﻿namespace _01_Recursive_Array_Sum
 {
     using System;
+    using System.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = Console.ReadLine()
+                        .Split(' ')
+                        .Select(int.Parse)
+                        .ToArray();
+
+            Console.WriteLine(PrintRecursionSum(arr, 0));
+        }
+
+        private static int PrintRecursionSum(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + PrintRecursionSum(arr, index + 1);
         }
     }
 }
