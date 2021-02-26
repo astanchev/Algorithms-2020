4f858683b4312a88b62cbb48554aa9bde09e35cc
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-02-26 14:49:46 +0200

    Add 02. Recursive Drawing - solution

--- 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/02_Recursive_Drawing/Program.cs
+++ 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/02_Recursive_Drawing/Program.cs
@@ -4,9 +4,28 @@
 
     class Program
     {
+        private static char firstChar = '*';
+        private static char secondChar = '#';
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintFigure(n);
+        }
+
+        private static void PrintFigure(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string(firstChar, n));
+
+            PrintFigure(n - 1);
+
+            Console.WriteLine(new string(secondChar, n));
         }
     }
 }
