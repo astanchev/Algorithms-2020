d9e7125b05f82a1c62241314b2600b3faa0d09fe
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-02-26 19:58:04 +0200

    Add 07. Recursive Fibonacci - solution with memoization, starting from 1

--- 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/07_Recursive_Fibonacci/Program.cs
+++ 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/07_Recursive_Fibonacci/Program.cs
@@ -1,12 +1,50 @@
 ﻿namespace _07_Recursive_Fibonacci
 {
     using System;
+    using System.Numerics;
 
     class Program
     {
+        private static BigInteger[] results;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            results = new BigInteger[n + 1];
+
+            FindFibbonacci(n);
+
+            PrintFibonacci(n);
+        }
+
+        private static void PrintFibonacci(int n)
+        {
+            Console.WriteLine(results[n]);
+        }
+
+        private static BigInteger FindFibbonacci(int n)
+        {
+            if (results[n] != 0)
+            {
+                return results[n];
+            }
+
+            if (n == 0)
+            {
+                results[n] = 1;
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                results[n] = 1;
+                return 1;
+            }
+
+            results[n] = FindFibbonacci(n - 1) + FindFibbonacci(n - 2);
+
+            return results[n];
         }
     }
 }
