dff8fd62e1102f189e2a72bfee68b3216ce83725
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-02-26 15:14:33 +0200

    Add 03. Generating 0/1 Vectors - solution

--- 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/03_Generating_0_1_Vectors/Program.cs
+++ 01. Recursion-and-Backtracking-Lab/01-Recursion-and-Backtracking-Lab/03_Generating_0_1_Vectors/Program.cs
@@ -6,7 +6,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintVectors(n);
+        }
+
+        private static void PrintVectors(int n)
+        {
+            int index = 0;
+            int[] vector = new int[n];
+
+            Gen01(index, vector);
+        }
+
+        private static void Gen01(int index, int[] vector)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join("", vector));
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    vector[index] = i;
+                    Gen01(index + 1, vector);
+                }
+            }
         }
     }
 }
