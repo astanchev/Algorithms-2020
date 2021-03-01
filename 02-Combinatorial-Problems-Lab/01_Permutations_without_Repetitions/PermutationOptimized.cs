f10850240a230b286062ecddbe3ede8cd1e95052
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-01 20:05:40 +0200

    Add 1. Permutations without Repetitions - solution

---
+++ 02-Combinatorial-Problems-Lab/01_Permutations_without_Repetitions/PermutationOptimized.cs
@@ -0,0 +1,47 @@
+﻿namespace _01_Permutations_without_Repetitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    class PermutationOptimized
+    {
+        private static string[] elements;
+        private static List<string> result;
+
+        static void Main()
+        {
+            elements = Console.ReadLine().Split();
+
+            result = new List<string>();
+
+            Permute(0);
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void Permute(int permutationsIndex)
+        {
+            if (permutationsIndex >= elements.Length)
+            {
+                result.Add(string.Join(' ', elements));
+                return;
+            }
+
+            Permute(permutationsIndex + 1);
+
+            for (int elementsIndex = permutationsIndex + 1; elementsIndex < elements.Length; elementsIndex++)
+            {
+                Swap(permutationsIndex, elementsIndex);
+                Permute(permutationsIndex + 1);
+                Swap(permutationsIndex, elementsIndex);
+            }
+        }
+
+        private static void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = temp;
+        }
+    }
+}
--- 02-Combinatorial-Problems-Lab/01_Permutations_without_Repetitions/Program.cs
+++ 02-Combinatorial-Problems-Lab/01_Permutations_without_Repetitions/Program.cs
@@ -1,12 +1,45 @@
 ﻿namespace _01_Permutations_without_Repetitions
 {
     using System;
+    using System.Collections.Generic;
 
     class Program
     {
-        static void Main(string[] args)
+        private static string[] elements;
+        private static string[] permutations;
+        private static List<string> result;
+        private static bool[] used;
+
+        //static void Main(string[] args)
+        //{
+        //    elements = Console.ReadLine().Split();
+        //    permutations = new string[elements.Length];
+        //    used = new bool[elements.Length];
+        //    result = new List<string>();
+
+        //    Permute(0);
+
+        //    Console.WriteLine(string.Join(Environment.NewLine, result));
+        //}
+
+        private static void Permute(int permutationsIndex)
         {
-            Console.WriteLine("Hello World!");
+            if (permutationsIndex >= elements.Length)
+            {
+                result.Add(string.Join(' ', permutations));
+                return;
+            }
+
+            for (int elementsIndex = 0; elementsIndex < elements.Length; elementsIndex++)
+            {
+                if (!used[elementsIndex])
+                {
+                    used[elementsIndex] = true;
+                    permutations[permutationsIndex] = elements[elementsIndex];
+                    Permute(permutationsIndex + 1);
+                    used[elementsIndex] = false;
+                }
+            }
         }
     }
 }
