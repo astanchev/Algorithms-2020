e3d0a7c4abcc4d27014024d067ecb9b118dd9889
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-01 20:26:40 +0200

    Add 02. Permutations with Repetition - solution

--- 02-Combinatorial-Problems-Lab/02_Permutations_with_Repetitions/Program.cs
+++ 02-Combinatorial-Problems-Lab/02_Permutations_with_Repetitions/Program.cs
@@ -1,12 +1,54 @@
 ﻿namespace _02_Permutations_with_Repetitions
 {
     using System;
+    using System.Collections.Generic;
 
     class Program
     {
-        static void Main(string[] args)
+        private static string[] elements;
+        private static List<string> result;
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            elements = Console.ReadLine().Split();
+
+            result = new List<string>();
+
+            Permute(0);
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void Permute(int permutationsIndex)
+        {
+            if (permutationsIndex >= elements.Length)
+            {
+                result.Add(string.Join(' ', elements));
+                return;
+            }
+
+            Permute(permutationsIndex + 1);
+
+            var swapped = new HashSet<string> { elements[permutationsIndex] };
+
+            for (int elementsIndex = permutationsIndex + 1; elementsIndex < elements.Length; elementsIndex++)
+            {
+                if (!swapped.Contains(elements[elementsIndex])) 
+                {
+                    Swap(permutationsIndex, elementsIndex);
+                    Permute(permutationsIndex + 1);
+                    Swap(permutationsIndex, elementsIndex);
+                    swapped.Add(elements[elementsIndex]);
+
+                }
+            }
+        }
+
+        private static void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = temp;
         }
     }
 }
