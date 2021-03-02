6f567c73ff9a22bb7d56acab457a00dec4755c28
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-02 14:17:18 +0200

    Add 1. Reverse Array - solution

--- 03-Recursion-and-Combinatorial-Problems-Exercise/01_Reverse_Array/ReverseArray.cs
+++ 03-Recursion-and-Combinatorial-Problems-Exercise/01_Reverse_Array/ReverseArray.cs
@@ -1,12 +1,60 @@
 ﻿namespace _01_Reverse_Array
 {
     using System;
+    using System.Linq;
 
     class ReverseArray
     {
+        private static int[] arr;
+        private static int[] result;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            arr = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            //result = new int[arr.Length];
+
+            //FillResult(arr.Length - 1, 0);
+
+            Reverse(0);
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        private static void Reverse(int startIndex)
+        {
+            if (startIndex >= arr.Length / 2)
+            {
+                return;
+            }
+
+            var endIndex = arr.Length - 1 - startIndex;
+
+            Swap(startIndex, endIndex);
+
+            Reverse(startIndex + 1);
+        }
+
+        private static void Swap(int startIndex, int endIndex)
+        {
+            var temp = arr[startIndex];
+            arr[startIndex] = arr[endIndex];
+            arr[endIndex] = temp;
+        }
+
+        private static void FillResult(int originalIdx, int resultIdx)
+        {
+            if (originalIdx < 0)
+            {
+                return;
+            }
+
+            result[resultIdx] = arr[originalIdx];
+
+            FillResult(originalIdx - 1, resultIdx + 1);
         }
     }
 }
--- 03-Recursion-and-Combinatorial-Problems-Exercise/08_School_Teams/SchoolTeams.cs
+++ 03-Recursion-and-Combinatorial-Problems-Exercise/08_School_Teams/SchoolTeams.cs
@@ -1,12 +1,11 @@
 ﻿namespace _08_School_Teams
 {
-    using System;
 
     class SchoolTeams
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
