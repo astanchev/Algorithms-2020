1573aecd76b8736aafef95ad7c7fe47442b62a08
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-02 15:00:47 +0200

    Add 4. Combinations without Repetition - solution

--- 03-Recursion-and-Combinatorial-Problems-Exercise/04_Combinations_without_Repetition/CombinationsWithoutRepetition.cs
+++ 03-Recursion-and-Combinatorial-Problems-Exercise/04_Combinations_without_Repetition/CombinationsWithoutRepetition.cs
@@ -1,12 +1,45 @@
 ﻿namespace _04_Combinations_without_Repetition
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class CombinationsWithoutRepetition
     {
+        private static int[] elements;
+        private static int[] combinations;
+        private static List<string> result;
+        private static int k;
+        private static int n;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            elements = Enumerable.Range(1, n).ToArray();
+
+            combinations = new int[k];
+            result = new List<string>();
+
+            Combinations(0, 0);
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void Combinations(int combIdx, int elementStartIdx)
+        {
+            if (combIdx >= combinations.Length)
+            {
+                result.Add(string.Join(' ', combinations));
+                return;
+            }
+
+            for (int i = elementStartIdx; i < elements.Length; i++)
+            {
+                combinations[combIdx] = elements[i];
+                Combinations(combIdx + 1, i + 1);
+            }
         }
     }
 }
