e03258ae55d5e822f79990778e9bf7a24aa2bd2e
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-02 09:01:16 +0200

    Add 6. Combinations with Repetition - solution

--- 02-Combinatorial-Problems-Lab/06_Combinations_with_Repetition/Program.cs
+++ 02-Combinatorial-Problems-Lab/06_Combinations_with_Repetition/Program.cs
@@ -1,12 +1,41 @@
 ﻿namespace _06_Combinations_with_Repetition
 {
     using System;
+    using System.Collections.Generic;
 
     class Program
     {
+        private static string[] elements;
+        private static string[] combinations;
+        private static List<string> result;
+        private static int k;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+            result = new List<string>();
+
+            Combinations(0, 0);
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void Combinations(int combIdx, int elementStartIdx)
+        {
+            if (combIdx >= combinations.Length)
+            {
+                result.Add(string.Join(' ', combinations));
+                return;
+            }
+
+            for (int i = elementStartIdx; i < elements.Length; i++)
+            {
+                combinations[combIdx] = elements[i];
+                Combinations(combIdx + 1, i);
+            }
         }
     }
 }
