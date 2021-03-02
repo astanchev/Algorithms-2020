ec7a6f9e76db9a97aa0e890d966da86049ecb254
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-02 09:05:23 +0200

    Add 7. N Choose K Count - solution

--- 02-Combinatorial-Problems-Lab/07_N_Choose_K_Count/Program.cs
+++ 02-Combinatorial-Problems-Lab/07_N_Choose_K_Count/Program.cs
@@ -6,7 +6,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(n, k));
+        }
+
+        private static long GetBinom(int n, int k)
+        {
+            if (n <= 1 || k == 0 || k == n)
+            {
+                return 1;
+            }
+
+            return GetBinom(n - 1, k) + GetBinom(n - 1, k - 1);
         }
     }
 }
