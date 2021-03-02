4b97e341e738a3ceeb717cfeba7b0b314b44b742
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-02 14:44:15 +0200

    Add 2. Nested Loops To Recursion - solution

--- 03-Recursion-and-Combinatorial-Problems-Exercise/02_Nested_Loops_To_Recursion/NestedLoops.cs
+++ 03-Recursion-and-Combinatorial-Problems-Exercise/02_Nested_Loops_To_Recursion/NestedLoops.cs
@@ -6,7 +6,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintLoops(n);
+        }
+
+        private static void PrintLoops(int n)
+        {
+            int index = 0;
+            int[] vector = new int[n];
+
+            GenerateLoops(index, vector);
+        }
+
+        private static void GenerateLoops(int index, int[] vector)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    vector[index] = i + 1;
+                    GenerateLoops(index + 1, vector);
+                }
+            }
         }
     }
 }
