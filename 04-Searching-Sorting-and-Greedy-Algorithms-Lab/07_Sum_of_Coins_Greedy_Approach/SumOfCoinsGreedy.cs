319a3ba7bffc7e76ebe05eb97d87549b1ac34d4c
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-05 19:21:45 +0200

    Add 07. Sum of Coins - solution with greedy approach

--- 04-Searching-Sorting-and-Greedy-Algorithms-Lab/07_Sum_of_Coins_Greedy_Approach/SumOfCoinsGreedy.cs
+++ 04-Searching-Sorting-and-Greedy-Algorithms-Lab/07_Sum_of_Coins_Greedy_Approach/SumOfCoinsGreedy.cs
@@ -1,12 +1,44 @@
 ﻿namespace _07_Sum_of_Coins_Greedy_Approach
 {
     using System;
+    using System.Linq;
+    using System.Text;
 
     class SumOfCoinsGreedy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var coins = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var target = int.Parse(Console.ReadLine());
+
+            var sortedCoins = coins.OrderByDescending(c => c).ToList();
+
+            var counter = 0;
+            var coinsIndex = 0;
+            var sb = new StringBuilder();
+
+            while (target > 0 && coinsIndex < sortedCoins.Count)
+            {
+                var currentCoin = sortedCoins[coinsIndex++];
+                var coinsCount = target / currentCoin;
+
+                if (coinsCount > 0)
+                {
+                    counter += coinsCount;
+                    target -= currentCoin * coinsCount;
+                    sb.AppendLine($"{coinsCount} coin(s) with value {currentCoin}");
+                }
+            }
+
+            if (target > 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Console.WriteLine($"Number of coins to take: {counter}");
+                Console.WriteLine(sb.ToString());
+            }
         }
     }
 }
