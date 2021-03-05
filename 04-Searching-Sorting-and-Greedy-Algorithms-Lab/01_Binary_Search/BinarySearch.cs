e94a652dcb2a7dfa6418a4a3d1f11b4b8b62ec2e
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-05 14:13:39 +0200

    Add 01. Binary Search - solution

--- 04-Searching-Sorting-and-Greedy-Algorithms-Lab/01_Binary_Search/BinarySearch.cs
+++ 04-Searching-Sorting-and-Greedy-Algorithms-Lab/01_Binary_Search/BinarySearch.cs
@@ -1,12 +1,64 @@
 ﻿namespace _01_Binary_Search
 {
     using System;
+    using System.Linq;
 
     class BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var searchedNumber = int.Parse(Console.ReadLine());
+
+            var index = BinarySearchRecursion(numbers, searchedNumber, 0, numbers.Length - 1);
+
+            Console.WriteLine(index);
+        }
+
+        private static object BinarySearchIteration(int[] numbers, int searchedNumber, int start, int end)
+        {
+            while (end >= start)
+            {
+                int middle = (start + end) / 2;
+
+                if (numbers[middle] > searchedNumber)
+                {
+                    return BinarySearchRecursion(numbers, searchedNumber, start, middle - 1);
+                }
+                else if (numbers[middle] < searchedNumber)
+                {
+                    return BinarySearchRecursion(numbers, searchedNumber, middle + 1, end);
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+
+            return -1;
+        }
+
+        private static object BinarySearchRecursion(int[] numbers, int searchedNumber, int start, int end)
+        {
+            if (end < start)
+            {
+                return -1;
+            }
+
+            int middle = (start + end) / 2;
+
+            if (numbers[middle] > searchedNumber)
+            {
+                return BinarySearchRecursion(numbers, searchedNumber, start, middle - 1);
+            }
+            else if (numbers[middle] < searchedNumber)
+            {
+                return BinarySearchRecursion(numbers, searchedNumber, middle + 1, end);
+            }
+            else
+            {
+                return middle;
+            }
         }
     }
 }
