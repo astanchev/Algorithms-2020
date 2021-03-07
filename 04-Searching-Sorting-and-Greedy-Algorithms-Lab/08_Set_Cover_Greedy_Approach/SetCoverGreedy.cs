f67aa190f153f0ee22fdc4c0f7e05c5aef03f9ab
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-07 12:00:41 +0200

    Add 08. Set Cover - greedy solution

--- 04-Searching-Sorting-and-Greedy-Algorithms-Lab/08_Set_Cover_Greedy_Approach/SetCoverGreedy.cs
+++ 04-Searching-Sorting-and-Greedy-Algorithms-Lab/08_Set_Cover_Greedy_Approach/SetCoverGreedy.cs
@@ -1,12 +1,63 @@
 ﻿namespace _08_Set_Cover_Greedy_Approach
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class SetCoverGreedy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> universe = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            int numSets = int.Parse(Console.ReadLine());
+
+            List<int[]> sets = new List<int[]>();
+            List<int[]> selectedSets = new List<int[]>();
+
+            FillSets(numSets, sets);
+            
+            FindSolution(universe, sets, selectedSets);
+
+            PrintSolution(selectedSets);
+
+        }
+
+        private static void PrintSolution(List<int[]> selectedSets)
+        {
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine(string.Join(", ", set));
+            }
+        }
+
+        private static void FindSolution(List<int> universe, List<int[]> sets, List<int[]> selectedSets)
+        {
+            while (universe.Count > 0)
+            {
+                int[] currentSet = sets
+                    .OrderByDescending(
+                                    s => s.Count(
+                                        e => universe.Contains(e)))
+                    .FirstOrDefault();
+
+                selectedSets.Add(currentSet);
+                sets.Remove(currentSet);
+
+                foreach (var e in currentSet)
+                {
+                    universe.Remove(e);
+                }
+            }
+        }
+
+        private static void FillSets(int numSets, List<int[]> sets)
+        {
+            for (int i = 0; i < numSets; i++)
+            {
+                int[] currentSet = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sets.Add(currentSet);
+            }
         }
     }
 }
