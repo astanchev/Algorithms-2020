8fffb1e65c1f1299c3a179122c272799ea24b786
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-17 13:47:06 +0200

    Add Subset Sum With Repetitions - solution

---
+++ 07-Introduction-to-Dynamic-Programming-Lab/06_Subset_Sum_With_Repetitions/SubsetSumWithRepetitions.cs
@@ -0,0 +1,71 @@
+﻿namespace _06_Subset_Sum_With_Repetitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    class SubsetSumWithRepetitions
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] { 3, 5, 2 };
+            var target = 17;
+
+            bool[] sums = CalcPossibleSums(nums, target);
+
+            Console.WriteLine(sums[target]);
+
+            var subset = FindSubset(nums, target, sums);
+
+            subset.Sort();
+
+            Console.WriteLine(string.Join(" ", subset));
+        }
+
+        static bool[] CalcPossibleSums(int[] nums, int targetSum)
+        {
+            var possible = new bool[targetSum + 1];
+            possible[0] = true;
+
+            for (int sum = 0; sum < possible.Length; sum++)
+            {
+                if (!possible[sum])
+                {
+                    continue;
+                }
+
+                foreach (var num in nums)
+                {
+                    var newSum = sum + num;
+
+                    if (newSum <= targetSum)
+                    {
+                        possible[newSum] = true;
+                    }
+                }
+            }
+
+            return possible;
+        }
+
+        static List<int> FindSubset(int[] nums, int targetSum, bool[] possibleSums)
+        {
+            var subset = new List<int>();
+            
+            while (targetSum > 0)
+            {
+                foreach (var num in nums)
+                {
+                    var newSum = targetSum - num;
+            
+                    if (newSum >= 0 && possibleSums[newSum])
+                    {
+                        targetSum = newSum;
+                        subset.Add(num);
+                    }
+                }
+            }
+
+            return subset;
+        }
+    }
+}
