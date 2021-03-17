21590d89f2966184c58c92ebf0816e471337b0fe
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-17 12:30:59 +0200

    Add Subset Sum - solution

---
+++ 07-Introduction-to-Dynamic-Programming-Lab/04_Subset_Sum/SubsetSum.cs
@@ -0,0 +1,56 @@
+﻿namespace _04_Subset_Sum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class SubsetSum
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] { 3, 5, 1, 4, 2 };
+            var target = 7;
+
+            var sums = GetAllPossibleSums(nums);
+
+            PrintAllPossibleSums(sums);
+
+            PrintIsSumContainsTarget(target, sums);
+        }
+
+        private static HashSet<int> GetAllPossibleSums(int[] nums)
+        {
+            var sums = new HashSet<int> { 0 };
+
+            foreach (var num in nums)
+            {
+                var newSums = new HashSet<int>();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = sum + num;
+                    newSums.Add(newSum);
+                }
+
+                sums.UnionWith(newSums);
+            }
+
+            if (!nums.Contains(0))
+            {
+                sums.Remove(0);
+            }
+
+            return sums;
+        }
+
+        private static void PrintIsSumContainsTarget(int target, HashSet<int> sums)
+        {
+            Console.WriteLine(sums.Contains(target));
+        }
+
+        private static void PrintAllPossibleSums(HashSet<int> sums)
+        {
+            Console.WriteLine(string.Join(" ", sums));
+        }
+    }
+}
---
+++ 07-Introduction-to-Dynamic-Programming-Lab/05_Subset_Sum_Without_Repetitions/SubsetSumWithoutRepetitions.cs
@@ -0,0 +1,67 @@
+﻿namespace _05_Subset_Sum_Without_Repetitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    class SubsetSumWithoutRepetitions
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] { 3, 5, 1, 4, 2 };
+            var target = 7;
+
+            var possibleSums = CalcPossibleSums(nums);
+
+            var firstPossibleSubset = FindSubset(target, possibleSums);
+
+            PrintFirstPossibleSubset(firstPossibleSubset);
+        }
+
+        private static Dictionary<int, int> CalcPossibleSums(int[] nums)
+        {
+            var possibleSums = new Dictionary<int, int> { { 0, 0 } };
+
+            foreach (var num in nums)
+            {
+                var newSums = new Dictionary<int, int>();
+
+                foreach (var sum in possibleSums.Keys)
+                {
+                    var newSum = sum + num;
+                    if (!possibleSums.ContainsKey(newSum))
+                    {
+                        newSums.Add(newSum, num);
+                    }
+                }
+
+                foreach (var sum in newSums)
+                {
+                    possibleSums.Add(sum.Key, sum.Value);
+                }
+            }
+
+            return possibleSums;
+        }
+
+        static List<int> FindSubset(int targetSum, IDictionary<int, int> possibleSums)
+        {
+            var subset = new List<int>();
+
+            while (targetSum > 0)
+            {
+                var lastNum = possibleSums[targetSum];
+                subset.Add(lastNum);
+                targetSum -= lastNum;
+            }
+
+            subset.Reverse();
+
+            return subset;
+        }
+
+        private static void PrintFirstPossibleSubset(List<int> firstPossibleSubset)
+        {
+            Console.WriteLine(string.Join(" ", firstPossibleSubset));
+        }
+    }
+}
