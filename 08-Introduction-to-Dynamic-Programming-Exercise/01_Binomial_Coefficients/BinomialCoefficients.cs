ff7264d8d2956406685999dfa6c9b321f778b53e
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-18 15:15:43 +0200

    Add 01. Binomial Coefficients - solution

--- 08-Introduction-to-Dynamic-Programming-Exercise/01_Binomial_Coefficients/BinomialCoefficients.cs
+++ 08-Introduction-to-Dynamic-Programming-Exercise/01_Binomial_Coefficients/BinomialCoefficients.cs
@@ -1,12 +1,40 @@
 ﻿namespace _01_Binomial_Coefficients
 {
     using System;
+    using System.Collections.Generic;
 
     class BinomialCoefficients
     {
+        private static Dictionary<string, long> cache;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            cache = new Dictionary<string, long>();
+
+            Console.WriteLine(GetBinom(n, k));
+        }
+
+        private static long GetBinom(int n, int k)
+        {
+            var id = $"{n} {k}";
+
+            if (cache.ContainsKey(id))
+            {
+                return cache[id];
+            }
+
+            if (n <= 1 || k == 0 || k == n)
+            {
+                return 1;
+            }
+
+            var result = GetBinom(n - 1, k) + GetBinom(n - 1, k - 1);
+            cache[id] = result;
+
+            return result;
         }
     }
 }
