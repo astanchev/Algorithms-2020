959fb755f747075adcf2056b422908af44374711
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-18 17:00:57 +0200

    Add 04. Sum with Limited Coins - solution

--- 08-Introduction-to-Dynamic-Programming-Exercise/04_Sum_with_Limited_Coins/SumWithLimitedCoins.cs
+++ 08-Introduction-to-Dynamic-Programming-Exercise/04_Sum_with_Limited_Coins/SumWithLimitedCoins.cs
@@ -1,12 +1,75 @@
 ﻿namespace _04_Sum_with_Limited_Coins
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class SumWithLimitedCoins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            var sums = CalcSums(numbers);
+            Console.WriteLine(sums[target]);
+
+            //var sumsCount = GetCount(numbers, target);
+            //Console.WriteLine(sumsCount);
+        }
+
+        private static Dictionary<int, int> CalcSums(int[] numbers)
+        {
+            var result = new Dictionary<int, int> {{ 0, 1 } };
+
+            foreach (var number in numbers)
+            {
+                var sums = result.Keys.ToArray();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = sum + number;
+
+                    if (!result.ContainsKey(newSum))
+                    {
+                        result[newSum] = 0;
+                    }
+
+                    result[newSum]++;
+                }
+            }
+
+            return result;
+        }
+
+        private static int GetCount(int[] numbers, int target)
+        {
+            var sums = new HashSet<int> { 0 };
+            var count = 0;
+
+            foreach (var number in numbers)
+            {
+                var newSums = new HashSet<int>();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = number + sum;
+                    newSums.Add(newSum);
+
+                    if (newSum == target)
+                    {
+                        count++;
+                    }
+                }
+
+                sums.UnionWith(newSums);
+            }
+
+            return count;
         }
     }
 }
