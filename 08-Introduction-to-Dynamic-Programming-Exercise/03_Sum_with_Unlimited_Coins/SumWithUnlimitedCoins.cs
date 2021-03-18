31bd42d08f5fbc92ce3ca672bcd883c3aaede3c3
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-18 18:04:30 +0200

    Add 05. Word Differences - solution

--- 08-Introduction-to-Dynamic-Programming-Exercise/03_Sum_with_Unlimited_Coins/SumWithUnlimitedCoins.cs
+++ 08-Introduction-to-Dynamic-Programming-Exercise/03_Sum_with_Unlimited_Coins/SumWithUnlimitedCoins.cs
@@ -1,12 +1,38 @@
 ﻿namespace _03_Sum_with_Unlimited_Coins
 {
     using System;
+    using System.Linq;
 
     class SumWithUnlimitedCoins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var coins = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            var count = GetCount(coins, target);
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetCount(int[] coins, int target)
+        {
+            var sums = new int[target + 1];
+            sums[0] = 1;
+
+            foreach (var coin in coins)
+            {
+                for (int sum = coin; sum < sums.Length; sum++)
+                {
+                    sums[sum] += sums[sum - coin];
+                }
+            }
+
+            return sums[target];
         }
     }
 }
--- 08-Introduction-to-Dynamic-Programming-Exercise/05_Word_Differences/Word_Differences.cs
+++ 08-Introduction-to-Dynamic-Programming-Exercise/05_Word_Differences/Word_Differences.cs
@@ -4,9 +4,76 @@
 
     class Word_Differences
     {
+        private static int[,] table;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            table = new int[str1.Length + 1, str2.Length + 1];
+
+            FillTable(str1, str2);
+
+            PrintResult(str1, str2);
+        }
+
+        private static void PrintResult(string str1, string str2)
+        {
+            Console.WriteLine($"Deletions and Insertions: {table[str1.Length, str2.Length]}");
+        }
+
+        private static void FillTable(string str1, string str2)
+        {
+            FillFirstRow();
+
+            FillFirstColumn();
+
+            FillRestOfTheTable(str1, str2);
+        }
+
+        private static void FillRestOfTheTable(string str1, string str2)
+        {
+            for (int row = 1; row < table.GetLength(0); row++)
+            {
+                for (int col = 1; col < table.GetLength(1); col++)
+                {
+                    if (str1[row - 1] == str2[col - 1])
+                    {
+                        table[row, col] = table[row - 1, col - 1];
+                    }
+                    else
+                    {
+                        table[row, col] = 1 + Math.Min(table[row, col - 1], table[row - 1, col]);
+                    }
+                }
+            }
+        }
+
+        private static void FillFirstColumn()
+        {
+            for (int col = 1; col < table.GetLength(1); col++)
+            {
+                table[0, col] = col;
+            }
+        }
+
+        private static void FillFirstRow()
+        {
+            for (int row = 1; row < table.GetLength(0); row++)
+            {
+                table[row, 0] = row;
+            }
         }
     }
 }
+
+//      ""  Y   M   C   A
+//  ""  0   1   2   3   4
+//  H   1   2   3   4   5
+//  M   2   3   2   3   4
+//  B   3   4   3   4   5
+//  B   4   5   4   5   6
+
+// If characters are different we take smaller between left and upper cell and add 1
+// If characters are same we take the left-upper diagonal cell
