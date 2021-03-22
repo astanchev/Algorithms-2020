69bba12518ac0c823b50f33ff2377cc93b3a8f7a
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-22 14:40:46 +0200

    Add 02. Paths - solution

--- 11-Exam-09-Jan-2021/01_Super_Set/SuperSet.cs
+++ 11-Exam-09-Jan-2021/01_Super_Set/SuperSet.cs
@@ -1,12 +1,51 @@
 ﻿namespace _01_Super_Set
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class SuperSet
     {
+        private static int[] elements;
+        private static List<string> result;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            elements = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            result = new List<string>();
+
+            GenerateSets();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void GenerateSets()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int[] combinations = new int[i+1];
+
+                Combinations(0, 0, combinations);
+            }
+        }
+
+        private static void Combinations(int combIdx, int elementStartIdx, int[] combinations)
+        {
+            if (combIdx >= combinations.Length)
+            {
+                result.Add(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementStartIdx; i < elements.Length; i++)
+            {
+                combinations[combIdx] = elements[i];
+                Combinations(combIdx + 1, i + 1, combinations);
+            }
         }
     }
 }
--- 11-Exam-09-Jan-2021/02_Paths/Paths.cs
+++ 11-Exam-09-Jan-2021/02_Paths/Paths.cs
@@ -1,12 +1,79 @@
 ﻿namespace _02_Paths
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class Paths
     {
+        private static Dictionary<int, List<int>> graph;
+        private static List<string> paths;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberNodes = int.Parse(Console.ReadLine());
+            graph = new Dictionary<int, List<int>>();
+            paths = new List<string>();
+
+            ReadGraph(numberNodes);
+
+            GeneratePaths(numberNodes);
+
+            Console.WriteLine(string.Join(Environment.NewLine, paths));
+        }
+
+        private static void GeneratePaths(int numberNodes)
+        {
+            for (int i = 0; i < numberNodes - 1; i++)
+            {
+                var path = new List<int>();
+                AddPaths(i, path);
+            }
+        }
+
+        private static void AddPaths(int start, List<int> path)
+        {
+            if (path.Contains(start))
+            {
+                return;
+            }
+
+            path.Add(start);
+
+            foreach (var child in graph[start])
+            {
+                AddPaths(child, path);
+
+                if (graph[child].Count == 0)
+                {
+                    paths.Add(string.Join(" ", path));
+                }
+                
+                var last = path.Last();
+                path.Remove(last);
+            }
+
+        }
+
+        private static void ReadGraph(int numberNodes)
+        {
+            for (int i = 0; i < numberNodes; i++)
+            {
+                graph[i] = new List<int>();
+                var childrenLine = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(childrenLine))
+                {
+                    var children = childrenLine
+                                        .Split(" ")
+                                        .Select(int.Parse);
+
+                    foreach (var child in children)
+                    {
+                        graph[i].Add(child);
+                    }
+                }
+            }
         }
     }
 }
