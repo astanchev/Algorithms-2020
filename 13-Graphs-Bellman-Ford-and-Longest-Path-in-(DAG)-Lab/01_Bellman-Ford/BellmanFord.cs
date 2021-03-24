f81fe5b54417319ac510b64bc7f6b7899da20cc7
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-24 17:40:41 +0200

    Add Graphs Bellman-Ford, Longest Path in (DAG) - Lab - sceleton

---
+++ 13-Graphs-Bellman-Ford-and-Longest-Path-in-(DAG)-Lab/01_Bellman-Ford/BellmanFord.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Bellman_Ford
+{
+    using System;
+
+    class BellmanFord
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 13-Graphs-Bellman-Ford-and-Longest-Path-in-(DAG)-Lab/02_Longest_Path/LongestPath.cs
@@ -0,0 +1,12 @@
+﻿namespace _02_Longest_Path
+{
+    using System;
+
+    class LongestPath
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
