b29e8627bd70005131d822c3fe9100a69a861b5b
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-25 10:13:04 +0200

    Add 01. Most Reliable Path and 05. Cable Network solutions

--- 14-Graphs-Bellman-Ford-Longest-Path-in-DAG-Exercise/01_Most_Reliable_Path/MostReliablePath.cs
+++ 14-Graphs-Bellman-Ford-Longest-Path-in-DAG-Exercise/01_Most_Reliable_Path/MostReliablePath.cs
@@ -1,12 +1,170 @@
 ﻿namespace _01_Most_Reliable_Path
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-    class MostReliablePath
+    using Wintellect.PowerCollections;
+
+    public class Edge
     {
-        static void Main(string[] args)
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class MostReliablePath
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+        private static double[] percentages;
+        private static int[] prev;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            edgesByNode = ReadGraph(edges);
+
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+
+            PrepareCollections();
+
+            percentages[start] = 100;
+
+            var queue = new OrderedBag<int>(
+                Comparer<int>.Create((f, s) => 
+                (int)(percentages[s] - percentages[f])));
+
+            queue.Add(start);
+
+            queue = FillNearestNodePath(end, queue);
+
+            PrintPath(end);
+        }
+
+        private static void PrintPath(int end)
+        {
+            if (percentages[end] == -1)
+            {
+                Console.WriteLine("There is no such path.");
+            }
+            else
+            {
+                Console.WriteLine($"Most reliable path reliability: {percentages[end]:F2}%");
+
+                Stack<int> path = RestorePath(end);
+
+                Console.WriteLine(string.Join(" -> ", path));
+            }
+        }
+
+        private static Stack<int> RestorePath(int end)
+        {
+            var path = new Stack<int>();
+
+            var node = end;
+
+            while (node != -1)
+            {
+                path.Push(node);
+                node = prev[node];
+            }
+
+            return path;
+        }
+
+        private static OrderedBag<int> FillNearestNodePath(int end, OrderedBag<int> queue)
+        {
+            while (queue.Count > 0)
+            {
+                var maxNode = queue.RemoveFirst();
+                var children = edgesByNode[maxNode];
+
+                if (maxNode == end || percentages[maxNode] == -1)
+                {
+                    break;
+                }
+
+                foreach (var child in children)
+                {
+                    var childNode = child.First == maxNode
+                        ? child.Second
+                        : child.First;
+
+                    if (percentages[childNode] == -1)
+                    {
+                        queue.Add(childNode);
+                    }
+
+                    var newDistance = child.Weight * percentages[maxNode] / 100;
+
+                    if (newDistance > percentages[childNode])
+                    {
+                        percentages[childNode] = newDistance;
+
+                        prev[childNode] = maxNode;
+
+                        queue = new OrderedBag<int>(
+                            queue,
+                            Comparer<int>.Create((f, s) => 
+                                (int)(percentages[s] - percentages[f])));
+                    }
+                }
+            }
+
+            return queue;
+        }
+
+        private static void PrepareCollections()
+        {
+            percentages = new double[edgesByNode.Count];
+            prev = new int[edgesByNode.Count];
+
+            percentages = Enumerable.Repeat<double>(-1, edgesByNode.Count).ToArray();
+            prev = Enumerable.Repeat<int>(-1, edgesByNode.Count).ToArray();
+        }
+
+        private static Dictionary<int, List<Edge>> ReadGraph(int e)
+        {
+            var result = new Dictionary<int, List<Edge>>();
+
+            for (int i = 0; i < e; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var firstNode = edgeData[0];
+                var secondNode = edgeData[1];
+                var weight = edgeData[2];
+
+                if (!result.ContainsKey(firstNode))
+                {
+                    result.Add(firstNode, new List<Edge>());
+                }
+
+                if (!result.ContainsKey(secondNode))
+                {
+                    result.Add(secondNode, new List<Edge>());
+                }
+
+                var edge = new Edge
+                {
+                    First = firstNode,
+                    Second = secondNode,
+                    Weight = weight
+                };
+
+                result[firstNode].Add(edge);
+                result[secondNode].Add(edge);
+            }
+
+            return result;
         }
     }
 }
--- 14-Graphs-Bellman-Ford-Longest-Path-in-DAG-Exercise/05_Cable_Network/CableNetwork.cs
+++ 14-Graphs-Bellman-Ford-Longest-Path-in-DAG-Exercise/05_Cable_Network/CableNetwork.cs
@@ -1,12 +1,131 @@
 ﻿namespace _05_Cable_Network
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-    class CableNetwork
+    using Wintellect.PowerCollections;
+
+    public class Edge
     {
-        static void Main(string[] args)
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class CableNetwork
+    {
+        private static Dictionary<int, List<Edge>> graph;
+        private static HashSet<int> spanningTree;
+        private static int totalBudget = 0;
+        private static int usedBudget = 0;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            totalBudget = int.Parse(Console.ReadLine());
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            graph = new Dictionary<int, List<Edge>>();
+            spanningTree = new HashSet<int>();
+
+            ReadGraph(edges);
+
+            Prim();
+
+            Console.WriteLine($"Budget used: {usedBudget}");
+        }
+
+        private static void Prim()
+        {
+            var queue = new OrderedBag<Edge>(
+                Comparer<Edge>.Create((f, s) => f.Weight - s.Weight));
+
+            queue.AddMany(spanningTree.SelectMany(n => graph[n]));
+
+            while (queue.Count > 0)
+            {
+                var minEdge = queue.RemoveFirst();
+
+                var nonTreeNode = GetNonTreeNode(minEdge.First, minEdge.Second);
+
+                if (nonTreeNode == -1)
+                {
+                    continue;
+                }
+
+                if (totalBudget >= minEdge.Weight)
+                {
+                    totalBudget -= minEdge.Weight;
+                    usedBudget += minEdge.Weight;
+                    spanningTree.Add(nonTreeNode);
+                    queue.AddMany(graph[nonTreeNode]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private static int GetNonTreeNode(int first, int second)
+        {
+            var nonTreeNode = -1;
+
+            if (spanningTree.Contains(first) &&
+                    !spanningTree.Contains(second))
+            {
+                nonTreeNode = second;
+            }
+            else if (spanningTree.Contains(second) &&
+                !spanningTree.Contains(first))
+            {
+                nonTreeNode = first;
+            }
+
+            return nonTreeNode;
+        }
+
+        private static void ReadGraph(int e)
+        {
+            for (int i = 0; i < e; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                var first = int.Parse(edgeData[0]);
+                var second = int.Parse(edgeData[1]);
+                var weight = int.Parse(edgeData[2]);
+
+                if (!graph.ContainsKey(first))
+                {
+                    graph.Add(first, new List<Edge>());
+                }
+
+                if (!graph.ContainsKey(second))
+                {
+                    graph.Add(second, new List<Edge>());
+                }
+
+                var edge = new Edge
+                {
+                    First = first,
+                    Second = second,
+                    Weight = weight
+                };
+
+                graph[first].Add(edge);
+                graph[second].Add(edge);
+
+                if (edgeData.Length > 3)
+                {
+                    spanningTree.Add(first);
+                    spanningTree.Add(second);
+                }
+            }
         }
     }
 }
