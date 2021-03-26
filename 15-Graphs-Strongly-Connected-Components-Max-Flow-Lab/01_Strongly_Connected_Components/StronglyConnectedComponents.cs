8ebca5186591762621e0ae58eb0c579d736f07d8
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-26 17:57:52 +0200

    Add 01. Strongly Connected Components - solution

--- 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/01_Strongly_Connected_Components/StronglyConnectedComponents.cs
+++ 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/01_Strongly_Connected_Components/StronglyConnectedComponents.cs
@@ -3,16 +3,100 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class StronglyConnectedComponents
     {
         private static List<List<int>> stronglyConnectedComponents;
+        private static List<int>[] graph;
+        private static List<int>[] reversedGraph;
+        private static bool[] visited;
+        private static Stack<int> stack = new Stack<int>();
 
         public static List<List<int>> FindStronglyConnectedComponents(List<int>[] targetGraph)
         {
-            throw new NotImplementedException();
+            graph = targetGraph;
+            visited = new bool[graph.Length];
+            stronglyConnectedComponents = new List<List<int>>();
+
+            BuildReverseGraph();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                Dfs(node);
+            }
+
+            visited = new bool[reversedGraph.Length];
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+
+                if (!visited[node])
+                {
+                    stronglyConnectedComponents.Add(new List<int>());
+                    ReverseDfs(node);
+                }
+            }
+
+            return stronglyConnectedComponents;
+        }
+
+        private static void BuildReverseGraph()
+        {
+            reversedGraph = new List<int>[graph.Length];
+
+            for (int node = 0; node < reversedGraph.Length; node++)
+            {
+                reversedGraph[node] = new List<int>();
+            }
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                foreach (var child in graph[node])
+                {
+                    reversedGraph[child].Add(node);
+                }
+            }
+        }
+
+        private static void ReverseDfs(int node)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in reversedGraph[node])
+            {
+                if (!visited[child])
+                {
+                    ReverseDfs(child);
+                }
+            }
+
+            stronglyConnectedComponents.Last().Add(node);
+        }
+
+        private static void Dfs(int node)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    Dfs(child);
+                }
+            }
+
+            stack.Push(node);
         }
     }
 }
--- 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/01_Strongly_Connected_Components/StronglyConnectedComponentsMain.cs
+++ 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/01_Strongly_Connected_Components/StronglyConnectedComponentsMain.cs
@@ -2,36 +2,64 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class StronglyConnectedComponentsMain
     {
         public static void Main(string[] args)
         {
-            var graph = new List<int>[]
-        {
-                new List<int>() {1, 11, 13}, // children of node 0
-                new List<int>() {6},         // children of node 1
-                new List<int>() {0},         // children of node 2
-                new List<int>() {4},         // children of node 3
-                new List<int>() {3, 6},      // children of node 4
-                new List<int>() {13},        // children of node 5
-                new List<int>() {0, 11},     // children of node 6
-                new List<int>() {12},        // children of node 7
-                new List<int>() {6, 11},     // children of node 8
-                new List<int>() {0},         // children of node 9
-                new List<int>() {4, 6, 10},  // children of node 10
-                new List<int>() {},          // children of node 11
-                new List<int>() {7},         // children of node 12
-                new List<int>() {2, 9},      // children of node 13
-        };
+            //    var graph = new List<int>[]
+            //{
+            //        new List<int>() {1, 11, 13}, // children of node 0
+            //        new List<int>() {6},         // children of node 1
+            //        new List<int>() {0},         // children of node 2
+            //        new List<int>() {4},         // children of node 3
+            //        new List<int>() {3, 6},      // children of node 4
+            //        new List<int>() {13},        // children of node 5
+            //        new List<int>() {0, 11},     // children of node 6
+            //        new List<int>() {12},        // children of node 7
+            //        new List<int>() {6, 11},     // children of node 8
+            //        new List<int>() {0},         // children of node 9
+            //        new List<int>() {4, 6, 10},  // children of node 10
+            //        new List<int>() {},          // children of node 11
+            //        new List<int>() {7},         // children of node 12
+            //        new List<int>() {2, 9},      // children of node 13
+            //};
+
+            List<int>[] graph = ReadInput();
 
             var result = StronglyConnectedComponents.FindStronglyConnectedComponents(graph);
 
             Console.WriteLine("Strongly Connected Components:");
             foreach (var component in result)
             {
-                Console.WriteLine("{{{0}}}", string.Join(", ", component));
+                component.Reverse();
+                Console.WriteLine($"{{{string.Join(", ", component)}}}");
             }
+        }
+
+        private static List<int>[] ReadInput()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            var result = new List<int>[nodes];
+            for (int node = 0; node < nodes; node++)
+            {
+                result[node] = new List<int>();
+            }
+
+            for (int i = 0; i < lines; i++)
+            {
+                int[] lineParts = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                result[lineParts[0]].AddRange(lineParts.Skip(1));
+            }
+
+            return result;
         }
     }
 }
