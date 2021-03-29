bc134b62ebfdf73f00de7f7f32f22c5d3950925b
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-29 09:46:38 +0300

    Add 03. Articulation Points - solution

--- 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/03_Articulation_Points/ArticulationPoints.cs
+++ 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/03_Articulation_Points/ArticulationPoints.cs
@@ -8,9 +8,69 @@
 
     public class ArticulationPoints
     {
+        private static List<int>[] graph;
+        private static List<int> articulationPoints;
+
+        private static bool[] visited;
+        private static int[] depths;
+        private static int[] lowpoint;
+        private static int?[] parent;
+
         public static List<int> FindArticulationPoints(List<int>[] targetGraph)
         {
-            throw new NotImplementedException();
+            graph = targetGraph;
+            visited = new bool[targetGraph.Length];
+            depths = new int[targetGraph.Length];
+            lowpoint = new int[targetGraph.Length];
+            parent = new int?[targetGraph.Length];
+            articulationPoints = new List<int>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (!visited[node])
+                {
+                    FindArticulationPoints(node, 1);
+                }
+            }
+
+            return articulationPoints;
+        }
+
+        private static void FindArticulationPoints(int node, int depth)
+        {
+            visited[node] = true;
+            depths[node] = depth;
+            lowpoint[node] = depth;
+
+            int countChildren = 0;
+            bool isArticulationPoint = false;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    parent[child] = node;
+                    FindArticulationPoints(child, depth + 1);
+                    countChildren++;
+
+                    if (lowpoint[child] >= depths[node])
+                    {
+                        isArticulationPoint = true;
+                    }
+
+                    lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+                }
+                else if (child != parent[node])
+                {
+                    lowpoint[node] = Math.Min(lowpoint[node], depths[child]);
+                }
+            }
+
+            if ((parent[node] != null && isArticulationPoint) ||
+                (parent[node] == null && countChildren > 1))
+            {
+                articulationPoints.Add(node);
+            }
         }
     }
 }
--- 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/03_Articulation_Points/ArticulationPointsMain.cs
+++ 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/03_Articulation_Points/ArticulationPointsMain.cs
@@ -2,30 +2,57 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class ArticulationPointsMain
     {
         public static void Main(string[] args)
         {
-            var graph = new List<int>[]
-        {
-                new List<int>() {1, 2, 6, 7, 9},      // children of node 0
-                new List<int>() {0, 6},               // children of node 1
-                new List<int>() {0, 7},               // children of node 2
-                new List<int>() {4},                  // children of node 3
-                new List<int>() {3, 6, 10},           // children of node 4
-                new List<int>() {7},                  // children of node 5
-                new List<int>() {0, 1, 4, 8, 10, 11}, // children of node 6
-                new List<int>() {0, 2, 5, 9},         // children of node 7
-                new List<int>() {6, 11},              // children of node 8
-                new List<int>() {0, 7},               // children of node 9
-                new List<int>() {4, 6},               // children of node 10
-                new List<int>() {6, 8},               // children of node 11
-        };
+            //var graph = new List<int>[]
+            //{
+            //    new List<int>() {1, 2, 6, 7, 9},      // children of node 0
+            //    new List<int>() {0, 6},               // children of node 1
+            //    new List<int>() {0, 7},               // children of node 2
+            //    new List<int>() {4},                  // children of node 3
+            //    new List<int>() {3, 6, 10},           // children of node 4
+            //    new List<int>() {7},                  // children of node 5
+            //    new List<int>() {0, 1, 4, 8, 10, 11}, // children of node 6
+            //    new List<int>() {0, 2, 5, 9},         // children of node 7
+            //    new List<int>() {6, 11},              // children of node 8
+            //    new List<int>() {0, 7},               // children of node 9
+            //    new List<int>() {4, 6},               // children of node 10
+            //    new List<int>() {6, 8},               // children of node 11
+            //};
 
-        var articulationPoints = ArticulationPoints.FindArticulationPoints(graph);
-        Console.WriteLine("Articulation points: " +
-            string.Join(", ", articulationPoints));
+            List<int>[] graph = ReadInput();
+
+            var articulationPoints = ArticulationPoints.FindArticulationPoints(graph);
+            Console.WriteLine("Articulation points: " +
+                string.Join(", ", articulationPoints));
+        }
+
+        private static List<int>[] ReadInput()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            var result = new List<int>[nodes];
+            for (int node = 0; node < nodes; node++)
+            {
+                result[node] = new List<int>();
+            }
+
+            for (int i = 0; i < lines; i++)
+            {
+                int[] lineParts = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                result[lineParts[0]].AddRange(lineParts.Skip(1));
+            }
+
+            return result;
         }
     }
 }
