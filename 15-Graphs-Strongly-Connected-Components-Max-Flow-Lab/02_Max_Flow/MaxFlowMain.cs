87e7caeea0bd32662b5e401a76a7e70dd117e425
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-29 11:55:28 +0300

    Add 02. Max Flow - solution

--- 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/02_Max_Flow/MaxFlowMain.cs
+++ 15-Graphs-Strongly-Connected-Components-Max-Flow-Lab/02_Max_Flow/MaxFlowMain.cs
@@ -1,23 +1,45 @@
 ﻿namespace _02_Max_Flow
 {
     using System;
+    using System.Linq;
 
     public class MaxFlowMain
     {
         public static void Main(string[] args)
         {
-            var graph = new int[][]
-        {
-                new int[] { 0, 10, 10, 0, 0, 0 },
-                new int[] { 0, 0, 2, 4, 8, 0},
-                new int[] { 0, 0, 0, 0, 9, 0},
-                new int[] { 0, 0, 0, 0, 0, 10 },
-                new int[] { 0, 0, 0, 6, 0, 10 },
-                new int[] { 0, 0, 0, 0, 0, 0 },
-        };
+            //var graph = new int[][]
+            //{
+            //    new int[] { 0, 10, 10, 0, 0, 0 },
+            //    new int[] { 0, 0, 2, 4, 8, 0},
+            //    new int[] { 0, 0, 0, 0, 9, 0},
+            //    new int[] { 0, 0, 0, 0, 0, 10 },
+            //    new int[] { 0, 0, 0, 6, 0, 10 },
+            //    new int[] { 0, 0, 0, 0, 0, 0 },
+            //};
 
-        var maxFlow = MaxFlow.FindMaxFlow(graph);
-        Console.WriteLine($"Max flow = {maxFlow}");
+            int[][] graph = ReadInput();
+
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            var maxFlow = MaxFlow.FindMaxFlow(graph, start, end);
+            Console.WriteLine($"Max flow = {maxFlow}");
+        }
+
+        private static int[][] ReadInput()
+        {
+            int nodes = int.Parse(Console.ReadLine());
+            int[][] result = new int[nodes][];
+
+            for (int i = 0; i < nodes; i++)
+            {
+                result[i] = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            return result;
         }
     }
 }
