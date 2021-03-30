6a593faa42136e6403190be0b989b3be331931af
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-30 19:58:47 +0300

    Add 02. Battle Points - solution

--- 18-Dynamic-Programming-Advanced-Exercise/02_Battle_Points/BattlePoints.cs
+++ 18-Dynamic-Programming-Advanced-Exercise/02_Battle_Points/BattlePoints.cs
@@ -1,12 +1,51 @@
 ﻿namespace _02_Battle_Points
 {
     using System;
+    using System.Linq;
 
     public class BattlePoints
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var requiredEnergy = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var battlePoints = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var enemies = requiredEnergy.Length;
+
+            var initialEnergy = int.Parse(Console.ReadLine());
+
+            var dp = new int[enemies + 1, initialEnergy + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                var enemyIdx = row - 1;
+                var enemyEnergy = requiredEnergy[enemyIdx];
+                var enemyBattlePoints = battlePoints[enemyIdx];
+
+                for (int energy = 1; energy < dp.GetLength(1); energy++)
+                {
+                    var skip = dp[row - 1, energy];
+
+                    if (enemyEnergy > energy)
+                    {
+                        dp[row, energy] = skip;
+                        continue;
+                    }
+
+                    var take = enemyBattlePoints + dp[row - 1, energy - enemyEnergy];
+
+                    dp[row, energy] = Math.Max(skip, take);
+                }
+            }
+
+            Console.WriteLine(dp[enemies, initialEnergy]);
         }
     }
 }
