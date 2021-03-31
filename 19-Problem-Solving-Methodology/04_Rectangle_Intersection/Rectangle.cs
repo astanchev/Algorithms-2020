a23fbfba154b76728195af41441d04c744e9560a
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-03-31 15:35:57 +0300

    Add  Rectangle Intersection - solution

---
+++ 19-Problem-Solving-Methodology/04_Rectangle_Intersection/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿namespace _04_Rectangle_Intersection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Rectangle : IComparable<Rectangle>
+    {
+        public Rectangle(int minX, int maxX, int minY, int maxY)
+        {
+            this.MinX = minX;
+            this.MaxX = maxX;
+            this.MinY = minY;
+            this.MaxY = maxY;
+        }
+
+        public int MinX { get; set; }
+
+        public int MaxX { get; set; }
+
+        public int MinY { get; set; }
+
+        public int MaxY { get; set; }
+
+        public int CompareTo(Rectangle other)
+        {
+            return this.MinX.CompareTo(other.MinX);
+        }
+
+        public decimal CalculatedArea()
+        {
+            return Math.Abs((this.MaxX - this.MinX) * (this.MaxY - this.MinY));
+        }
+    }
+}
--- 19-Problem-Solving-Methodology/04_Rectangle_Intersection/RectangleIntersection.cs
+++ 19-Problem-Solving-Methodology/04_Rectangle_Intersection/RectangleIntersection.cs
@@ -1,12 +1,123 @@
 ﻿namespace _04_Rectangle_Intersection
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-    class RectangleIntersection
+    public class RectangleIntersection
     {
-        static void Main(string[] args)
+        private static List<Rectangle> rectangles;
+        private static List<int> xCoordinates;
+        private static List<Rectangle>[] rectanglesByXCoordinates;
+        private static long result = 0;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            rectangles = new List<Rectangle>();
+            xCoordinates = new List<int>();
+
+            ReadInput(n);
+
+            ArrangeRectsByX();
+
+            for (int rectXIndex = 0; rectXIndex < rectanglesByXCoordinates.Count(); rectXIndex++)
+            {
+                if (rectanglesByXCoordinates[rectXIndex].Count() < 2)
+                {
+                    continue;
+                }
+
+                var yCoordinates = FillYCoordinates(rectXIndex);
+
+                yCoordinates.Sort();
+                var overlapped = FindOverlapped(rectXIndex, yCoordinates);
+
+                for (int rectYIndex = 0; rectYIndex < overlapped.Count(); rectYIndex++)
+                {
+                    if (overlapped[rectYIndex] >= 2)
+                    {
+                        int xSide = xCoordinates[rectXIndex + 1] - xCoordinates[rectXIndex];
+                        int ySide = yCoordinates[rectYIndex + 1] - yCoordinates[rectYIndex];
+
+                        result += xSide * ySide;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int[] FindOverlapped(int rectXIndex, List<int> yCoordinates)
+        {
+            int[] overlapped = new int[yCoordinates.Count - 1];
+
+            foreach (var rectangle in rectanglesByXCoordinates[rectXIndex])
+            {
+                for (int rectYIndex = 0; rectYIndex < yCoordinates.Count; rectYIndex++)
+                {
+                    if (rectangle.MaxY <= yCoordinates[rectYIndex] || rectangle.MinY >= yCoordinates[rectYIndex + 1])
+                    {
+                        continue;
+                    }
+                    overlapped[rectYIndex]++;
+                }
+            }
+
+            return overlapped;
+        }
+
+        private static List<int> FillYCoordinates(int rectIndex)
+        {
+            List<int> yCoordinates = new List<int>();
+
+            foreach (var rectangle in rectanglesByXCoordinates[rectIndex])
+            {
+                yCoordinates.Add(rectangle.MinY);
+                yCoordinates.Add(rectangle.MaxY);
+            }
+
+            return yCoordinates;
+        }
+
+        private static void ArrangeRectsByX()
+        {
+            xCoordinates.Sort();
+            rectanglesByXCoordinates = new List<Rectangle>[xCoordinates.Count - 1];
+
+            for (int i = 0; i < xCoordinates.Count - 1; i++)
+            {
+                rectanglesByXCoordinates[i] = new List<Rectangle>();
+            }
+
+            foreach (var rectangle in rectangles)
+            {
+                for (int i = 0; i < rectanglesByXCoordinates.Count(); i++)
+                {
+                    if (rectangle.MaxX > xCoordinates[i] && rectangle.MinX < xCoordinates[i + 1])
+                    {
+                        rectanglesByXCoordinates[i].Add(rectangle);
+                    }
+                }
+            }
+        }
+
+        private static void ReadInput(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var coordinates = Console.ReadLine()
+                                            .Split(' ')
+                                            .Select(int.Parse)
+                                            .ToArray();
+                xCoordinates.Add(coordinates[0]);
+                xCoordinates.Add(coordinates[1]);
+
+                var rectangle = new Rectangle(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+
+                rectangles.Add(rectangle);
+            }
         }
     }
 }
