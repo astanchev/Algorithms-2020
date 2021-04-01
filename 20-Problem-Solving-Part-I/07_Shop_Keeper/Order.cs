3e001524cd4ca7962a4955f5b72724bef2b47d25
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-01 17:21:42 +0300

    Add 07. Shop Keeper - solution

---
+++ 20-Problem-Solving-Part-I/07_Shop_Keeper/Order.cs
@@ -0,0 +1,50 @@
+﻿namespace _07_Shop_Keeper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Order : IComparable<Order>
+    {
+        private static int dummyId = int.MaxValue;
+
+        public int Id { get; set; }
+
+        public Stack<int> Indexes { get; set; }
+
+        public int PeekNextIndex()
+        {
+            if (this.Indexes.Count <= 0)
+            {
+                this.Indexes.Push(dummyId--);
+            }
+
+            return this.Indexes.Peek();
+        }
+
+        public Order(int id)
+        {
+            this.Id = id;
+            this.Indexes = new Stack<int>();
+        }
+
+        public Order()
+        {
+            this.Indexes = new Stack<int>();
+            this.Indexes.Push(dummyId);
+            this.Id = dummyId--;
+        }
+
+        public int CompareTo(Order other)
+        {
+            return this.PeekNextIndex().CompareTo(other.PeekNextIndex());
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Id} : {string.Join(", ", this.Indexes)}";
+        }
+    }
+}
--- 20-Problem-Solving-Part-I/07_Shop_Keeper/ShopKeeper.cs
+++ 20-Problem-Solving-Part-I/07_Shop_Keeper/ShopKeeper.cs
@@ -1,12 +1,85 @@
 ﻿namespace _07_Shop_Keeper
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-    class ShopKeeper
+    public class ShopKeeper
     {
-        static void Main(string[] args)
+        private static Dictionary<int, Order> orders;
+        private static int[] initialStock;
+        private static int[] ordersList;
+        private static SortedSet<Order> shop;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            initialStock = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            ordersList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            if (!initialStock.Contains(ordersList[0]))
+            {
+                Console.WriteLine("impossible");
+                return;
+            }
+
+            orders = new Dictionary<int, Order>();
+            FillOrders();
+
+            shop = new SortedSet<Order>();
+            FillShop();
+
+            var stockChanges = 0;
+
+            foreach (var order in ordersList)
+            {
+                var currentOrder = orders[order];
+
+                if (!shop.Contains(currentOrder))
+                {
+                    // Console.WriteLine($"{shop.Last().Id} <<< {order}");
+                    shop.Remove(shop.Last());
+                    shop.Add(currentOrder);
+                    stockChanges++;
+                }
+
+                shop.Remove(currentOrder);
+                currentOrder.Indexes.Pop();
+                shop.Add(currentOrder);
+            }
+
+            Console.WriteLine(stockChanges);
+        }
+
+        private static void FillShop()
+        {
+            foreach (var product in initialStock.Distinct())
+            {
+                shop.Add(!orders.ContainsKey(product) ?
+                                            new Order() :
+                                            orders[product]);
+            }
+        }
+
+        private static void FillOrders()
+        {
+            for (var index = ordersList.Length - 1; index >= 0; index--)
+            {
+                var order = ordersList[index];
+
+                if (!orders.ContainsKey(order))
+                {
+                    orders.Add(order, new Order(order));
+                }
+
+                orders[order].Indexes.Push(index);
+            }
         }
     }
 }
