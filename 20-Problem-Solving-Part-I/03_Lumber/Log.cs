84e7b40f7e7b60a5378f2ded32e7938eee1b87b4
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-01 15:39:24 +0300

    Add 03. Lumber - solution

---
+++ 20-Problem-Solving-Part-I/03_Lumber/Log.cs
@@ -0,0 +1,44 @@
+﻿namespace _03_Lumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Log
+    {
+        private readonly int x1;
+        private readonly int y1;
+        private readonly int x2;
+        private readonly int y2;
+
+        public Log(string coordinatesStr, int index)
+        {
+            var coordinates = coordinatesStr
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            this.x1 = Math.Min(coordinates[0], coordinates[2]);
+            this.y1 = Math.Max(coordinates[1], coordinates[3]);
+            this.x2 = Math.Max(coordinates[0], coordinates[2]);
+            this.y2 = Math.Min(coordinates[1], coordinates[3]);
+
+            this.Index = index;
+        }
+
+        public int Index { get; }
+
+        public bool Overlap(Log other)
+        {
+            return this.x1 <= other.x2 && other.x1 <= this.x2 &&
+                   this.y1 >= other.y2 && other.y1 >= this.y2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Index}: {this.x1} {this.y1}, {this.x2} {this.y2}";
+        }
+    }
+}
--- 20-Problem-Solving-Part-I/03_Lumber/Lumber.cs
+++ 20-Problem-Solving-Part-I/03_Lumber/Lumber.cs
@@ -1,12 +1,102 @@
 ﻿namespace _03_Lumber
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
-    class Lumber
+    public class Lumber
     {
-        static void Main(string[] args)
+        private static int count;
+        private static List<Log> logs;
+        private static List<int>[] graph;
+        private static bool[] visited;
+        private static int[] countConnected;
+        private static List<string> result;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var logsCount = input[0];
+            var queriesCount = input[1];
+
+            logs = new List<Log>();
+            graph = new List<int>[logsCount + 1];
+            result = new List<string>();
+
+            CreateLogGraph(logsCount);
+
+            visited = new bool[logsCount + 1];
+            countConnected = new int[logsCount + 1];
+
+            for (var logIndex = 1; logIndex <= logsCount; logIndex++)
+            {
+                if (visited[logIndex])
+                {
+                    continue;
+                }
+
+                DFS(logIndex);
+                count++;
+            }
+
+            CheckQueries(queriesCount);
+
+            Console.Write(string.Join(Environment.NewLine, result));
+        }
+
+        private static void CheckQueries(int queriesCount)
+        {
+            for (var i = 0; i < queriesCount; i++)
+            {
+                var query = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                result.Add(countConnected[query[0]] == countConnected[query[1]] ? "YES" : "NO");
+            }
+        }
+
+        private static void CreateLogGraph(int logsCount)
+        {
+            for (var i = 1; i <= logsCount; i++)
+            {
+                var currentLog = new Log(Console.ReadLine(), i);
+
+                graph[i] = new List<int>();
+
+                foreach (var log in logs)
+                {
+                    if (!log.Overlap(currentLog))
+                    {
+                        continue;
+                    }
+
+                    graph[log.Index].Add(i);
+                    graph[i].Add(log.Index);
+                }
+
+                logs.Add(currentLog);
+            }
+        }
+
+        private static void DFS(int log)
+        {
+            visited[log] = true;
+            countConnected[log] = count;
+
+            foreach (var child in graph[log])
+            {
+                if (!visited[child])
+                {
+                    DFS(child);
+                }
+            }
         }
     }
 }
