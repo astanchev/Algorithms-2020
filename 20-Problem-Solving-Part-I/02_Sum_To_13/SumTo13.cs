39d1702c573d2a9339edb281938c792d371ebeb6
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-01 15:11:10 +0300

    Add 02. Sum To 13 - solution

--- 20-Problem-Solving-Part-I/02_Sum_To_13/SumTo13.cs
+++ 20-Problem-Solving-Part-I/02_Sum_To_13/SumTo13.cs
@@ -1,12 +1,38 @@
 ﻿namespace _02_Sum_To_13
 {
     using System;
+    using System.Linq;
 
     class SumTo13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int targetNumber = 13;
+
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int a = numbers[0];
+            int b = numbers[1];
+            int c = numbers[2];
+
+            if (a + b + c == targetNumber ||
+                a + b - c == targetNumber ||
+                a - b + c == targetNumber ||
+                a - b - c == targetNumber ||
+                -a + b + c == targetNumber ||
+                -a - b + c == targetNumber ||
+                -a + b - c == targetNumber ||
+                -a - b - c == targetNumber)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
