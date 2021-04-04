7b923ae6fd90dfe7e66bd4b9c98e565738e19598
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-04 17:51:11 +0300

    Add 02. Travelling Policeman - solution

---
+++ 22-Exam-Preparation/02_Travelling_Policeman/Item.cs
@@ -0,0 +1,35 @@
+﻿namespace _02_Travelling_Policeman
+{
+    public class Item
+    {
+        private string name;
+        private int damage;
+        private int count;
+        private int length;
+
+        public Item()
+        {
+        }
+
+        public Item(string name, int damage, int count, int length)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.count = count;
+            this.length = length;
+        }
+
+        public string Name { get => this.name; set => this.name = value; }
+
+        public int Damage { get => this.damage; set => this.damage = value; }
+
+        public int Count { get => this.count; set => this.count = value; }
+
+        public int Length { get => this.length; set => this.length = value; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
--- 22-Exam-Preparation/02_Travelling_Policeman/TravellingPoliceman.cs
+++ 22-Exam-Preparation/02_Travelling_Policeman/TravellingPoliceman.cs
@@ -1,12 +1,107 @@
 ﻿namespace _02_Travelling_Policeman
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
     public class TravellingPoliceman
     {
+        private static StringBuilder builder = new StringBuilder();
+        private static List<Item> items;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fuel = int.Parse(Console.ReadLine());
+
+            items = new List<Item>();
+
+            ReadInput();
+
+            List<Item> total = new List<Item>();
+            total = Fill(fuel);
+
+            total.Reverse();
+
+            builder.AppendLine(String.Join(" -> ", total.Select(i => i.Name)))
+                .AppendLine($"Total pokemons caught -> {total.Sum(i => i.Count)}")
+                .AppendLine($"Total car damage -> {total.Sum(i => i.Damage)}")
+                .Append($"Fuel Left -> {fuel - total.Sum(i => i.Length)}");
+
+            Console.WriteLine(builder.ToString());
+        }
+
+        private static List<Item> Fill(int fuel)
+        {
+            int[,] values = new int[items.Count + 1, fuel + 1];
+            bool[,] isItemIncluded = new bool[items.Count + 1, fuel + 1];
+            List<Item> itemsTaken = new List<Item>();
+
+            for (int itemIndex = 0; itemIndex < items.Count; itemIndex++)
+            {
+                int row = itemIndex + 1;
+                Item item = items[itemIndex];
+
+                for (int c = 1; c < fuel; c++)
+                {
+                    int excludedValue = values[row - 1, c];
+                    int includedValue = 0;
+
+                    if (item.Length <= c)
+                    {
+                        includedValue = (item.Count * 10 - item.Damage) + values[row - 1, c - item.Length];
+                    }
+
+                    if (includedValue > excludedValue)
+                    {
+                        values[row, c] = includedValue;
+                        isItemIncluded[row, c] = true;
+                    }
+                    else
+                    {
+                        values[row, c] = excludedValue;
+                    }
+                }
+            }
+
+            int tempCap = fuel;
+
+            for (int i = items.Count - 1; i > 0; i--)
+            {
+                if (!isItemIncluded[i, tempCap])
+                {
+                    continue;
+                }
+
+                Item item = items[i - 1];
+                itemsTaken.Add(item);
+
+                tempCap = tempCap - item.Length;
+            }
+
+            return itemsTaken;
+        }
+
+        private static void ReadInput()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(',').ToArray();
+
+                Item item = new Item(tokens[0],
+                                int.Parse(tokens[1]),
+                                int.Parse(tokens[2]),
+                                int.Parse(tokens[3]));
+
+                items.Add(item);
+            }
         }
     }
 }
