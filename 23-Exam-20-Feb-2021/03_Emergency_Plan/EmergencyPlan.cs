d55e26ec01c10d32b20f4c07c08b489f5e56b294
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-05 12:47:40 +0300

    Refactor 03. Emergency Plan

--- 23-Exam-20-Feb-2021/03_Emergency_Plan/EmergencyPlan.cs
+++ 23-Exam-20-Feb-2021/03_Emergency_Plan/EmergencyPlan.cs
@@ -41,132 +41,169 @@
 
             var end = ExitRooms;
 
-            for (int i = 0; i < nodesCount; i++)
+            for (int room = 0; room < nodesCount; room++)
             {
-                if (ExitRooms.Contains(i))
+                if (ExitRooms.Contains(room))
                 {
                     continue;
                 }
 
-                if (!edgesByNode.ContainsKey(i))
+                if (!edgesByNode.ContainsKey(room))
                 {
-                    Console.WriteLine($"Unreachable {i} (N/A)");
+                    Console.WriteLine($"Unreachable {room} (N/A)");
                     continue;
                 }
 
-                var start = i;
+                var start = room;
 
                 var maxNode = edgesByNode.Keys.Max();
 
-                var distances = new int[edgesCount + 1];
-                for (int j = 0; j < distances.Length; j++)
-                {
-                    distances[j] = int.MaxValue;
-                }
+                int[] distances = FillDistances(edgesCount);
+
+                FillPrev(nodesCount);
 
                 distances[start] = 0;
-
-                prev = new int[nodesCount + 1];
-                for (int j = 0; j < prev.Length; j++)
-                {
-                    prev[j] = int.MaxValue;
-                }
-
                 prev[start] = -1;
 
-                var queue = new OrderedBag<int>(Comparer<int>.Create((f, s) => distances[f] - distances[s]));
-                queue.Add(start);
+                CalculateDistances(end, start, distances);
 
-                while (queue.Count > 0)
-                {
-                    var minNode = queue.RemoveFirst();
+                int minTimeExit = FindMinTimeToExit(end, distances);
 
-                    var children = edgesByNode[minNode];
-
-                    if (end.Contains(minNode))
-                    {
-                        break;
-                    }
-
-                    foreach (var child in children)
-                    {
-                        var childNode = child.First == minNode ? child.Second : child.First;
-
-                        if (distances[childNode] == int.MaxValue)
-                        {
-                            queue.Add(childNode);
-                        }
-
-                        var newDistance = child.Weight + distances[minNode];
-
-                        if (newDistance < distances[childNode])
-                        {
-                            distances[childNode] = newDistance;
-                            prev[childNode] = minNode;
-                            queue = new OrderedBag<int>(queue, Comparer<int>.Create((f, s) => distances[f] - distances[s]));
-                        }
-                    }
-                }
-
-                var minTimeExit = 0;
-
-                if (end.Length > 1)
-                {
-                    var timeOfEnds = new List<int>();
-
-                    foreach (var end1 in end)
-                    {
-                        timeOfEnds.Add(distances[end1]);
-                    }
-
-                    minTimeExit = timeOfEnds.Min();
-                }
-                else
-                {
-                    minTimeExit = distances[end[0]];
-                }
-
-                var isThereExit = false;
-
-                foreach (var end1 in end)
-                {
-                    isThereExit = prev[end1] != int.MaxValue;
-
-                    if (isThereExit == true)
-                    {
-                        break;
-                    }
-                }
+                bool isThereExit = HasExit(end);
 
                 if (minTimeExit < 1)
                 {
                     continue;
                 }
 
-                if (isThereExit)
+                minTimeExit = PrintRoomDetails(timeInSecondsToSafeExit, room, minTimeExit, isThereExit);
+            }
+        }
+
+        private static int PrintRoomDetails(int timeInSecondsToSafeExit, int room, int minTimeExit, bool isThereExit)
+        {
+            if (isThereExit)
+            {
+                if (minTimeExit > timeInSecondsToSafeExit)
                 {
-                    if (minTimeExit > timeInSecondsToSafeExit)
-                    {
-                        int hours = minTimeExit / 3600;
-                        int mins = (minTimeExit % 3600) / 60;
-                        minTimeExit = minTimeExit % 60;
+                    int hours = minTimeExit / 3600;
+                    int mins = (minTimeExit % 3600) / 60;
+                    minTimeExit = minTimeExit % 60;
 
-                        Console.WriteLine($"Unsafe {i} ({hours:D2}:{mins:D2}:{minTimeExit:D2})");
-                    }
-                    else
-                    {
-                        int hours = minTimeExit / 3600;
-                        int mins = (minTimeExit % 3600) / 60;
-                        minTimeExit = minTimeExit % 60;
-
-                        Console.WriteLine($"Safe {i} ({hours:D2}:{mins:D2}:{minTimeExit:D2})");
-                    }
+                    Console.WriteLine($"Unsafe {room} ({hours:D2}:{mins:D2}:{minTimeExit:D2})");
                 }
                 else
                 {
-                    Console.WriteLine($"Unreachable {i} (N/A)");
+                    int hours = minTimeExit / 3600;
+                    int mins = (minTimeExit % 3600) / 60;
+                    minTimeExit = minTimeExit % 60;
+
+                    Console.WriteLine($"Safe {room} ({hours:D2}:{mins:D2}:{minTimeExit:D2})");
                 }
             }
+            else
+            {
+                Console.WriteLine($"Unreachable {room} (N/A)");
+            }
+
+            return minTimeExit;
+        }
+
+        private static bool HasExit(int[] end)
+        {
+            var isThereExit = false;
+
+            foreach (var end1 in end)
+            {
+                isThereExit = prev[end1] != int.MaxValue;
+
+                if (isThereExit == true)
+                {
+                    break;
+                }
+            }
+
+            return isThereExit;
+        }
+
+        private static int FindMinTimeToExit(int[] end, int[] distances)
+        {
+            var minTimeExit = 0;
+
+            if (end.Length > 1)
+            {
+                var timeOfEnds = new List<int>();
+
+                foreach (var end1 in end)
+                {
+                    timeOfEnds.Add(distances[end1]);
+                }
+
+                minTimeExit = timeOfEnds.Min();
+            }
+            else
+            {
+                minTimeExit = distances[end[0]];
+            }
+
+            return minTimeExit;
+        }
+
+        private static void CalculateDistances(int[] end, int start, int[] distances)
+        {
+            var queue = new OrderedBag<int>(Comparer<int>.Create((f, s) => distances[f] - distances[s]));
+            queue.Add(start);
+
+            while (queue.Count > 0)
+            {
+                var minNode = queue.RemoveFirst();
+
+                var children = edgesByNode[minNode];
+
+                if (end.Contains(minNode))
+                {
+                    break;
+                }
+
+                foreach (var child in children)
+                {
+                    var childNode = child.First == minNode ? child.Second : child.First;
+
+                    if (distances[childNode] == int.MaxValue)
+                    {
+                        queue.Add(childNode);
+                    }
+
+                    var newDistance = child.Weight + distances[minNode];
+
+                    if (newDistance < distances[childNode])
+                    {
+                        distances[childNode] = newDistance;
+                        prev[childNode] = minNode;
+                        queue = new OrderedBag<int>(queue, Comparer<int>.Create((f, s) => distances[f] - distances[s]));
+                    }
+                }
+            }
+        }
+
+        private static void FillPrev(int nodesCount)
+        {
+            prev = new int[nodesCount + 1];
+            for (int j = 0; j < prev.Length; j++)
+            {
+                prev[j] = int.MaxValue;
+            }
+        }
+
+        private static int[] FillDistances(int edgesCount)
+        {
+            var distances = new int[edgesCount + 1];
+            for (int j = 0; j < distances.Length; j++)
+            {
+                distances[j] = int.MaxValue;
+            }
+
+            return distances;
         }
 
         private static Dictionary<int, List<Edge>> ReadGraph(int e)
